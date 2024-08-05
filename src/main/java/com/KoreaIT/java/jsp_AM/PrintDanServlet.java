ee2d601c490a53d01c9e76f0a90b5acacb408fd5
meustar <meustar829@gmail.com>
2024-08-05 15:37:31 +0900

    request.getParameter를 이용한 매개변수로 원하는 구구단 출력하기

--- src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
@@ -17,13 +17,25 @@ public class PrintDanServlet extends HttpServlet {
 
 		response.setContentType("text/html;charset=UTF-8");
 		
-		response.getWriter().append("==8단==<br>");
+//		5교시
 		
-		int dan = 8;
-		for (int i = 1; i <= 9; i++)
-		response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan*i));
+//		response.getWriter().append("==구구단==<br>");
+		
+//		int dan = 8;
+//		for (int i = 1; i <= 9; i++)
+//		response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan*i));
 		
 //		\n은 개행이 먹히지 않는다. 줄바꿈을 하려면 html의 <br>태그를 이용해야 줄바꿈이 된다 => java와 HTML의 융합!
 		
+		
+		
+		
+//		http://localhost:8080/JSP_AM_2024_08/printDan?dan=8		=> 8단 출력
+//		http://localhost:8080/JSP_AM_2024_08/printDan?dan=9		=> 9단 출력
+		int dan = Integer.parseInt(request.getParameter("dan"));
+		response.getWriter().append(String.format("==%d 단==<br>", dan));
+		
+		for (int i = 1; i <= 9; i++)
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
 	}
 }
