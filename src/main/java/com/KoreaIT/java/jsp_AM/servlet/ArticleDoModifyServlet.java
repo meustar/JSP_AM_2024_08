126d19223d207d23e93fee35b2c46b4ba492c58e
meustar <meustar829@gmail.com>
2024-08-08 11:35:34 +0900

    게시글 수정시 작성자에게  권한부여

--- src/main/java/com/KoreaIT/java/jsp_AM/servlet/ArticleDoModifyServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/servlet/ArticleDoModifyServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 import com.KoreaIT.java.jsp_AM.util.DBUtil;
 import com.KoreaIT.java.jsp_AM.util.SecSql;
@@ -13,6 +14,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/article/doModify")
 public class ArticleDoModifyServlet extends HttpServlet {
@@ -20,6 +22,13 @@ public class ArticleDoModifyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
+		
+		HttpSession session = request.getSession();
+		
+		if (session.getAttribute("loginedMemberId") == null) {
+			response.getWriter().append(String.format("<script>alert('로그인 해주세요.'); location.replace('../member/login');</script>"));
+			return;
+		}
 
 		// DB 연결
 		try {
@@ -42,11 +51,24 @@ public class ArticleDoModifyServlet extends HttpServlet {
 
 			int id = Integer.parseInt(request.getParameter("id"));
 			
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
+			
+			Map<String, Object> articleRow = DBUtil.selectRow(conn, sql);
+			
+			int loginedMemberId = (int) session.getAttribute("loginedMemberId");
+			
+			if (loginedMemberId != (int) articleRow.get("memberId")) {
+				response.getWriter().append(
+						String.format("<script>alert('%d번 글에대한 수정권한이 없습니다.'); location.replace('list');</script>", id));
+				return;
+			}
+
 			String title = request.getParameter("title");
 			String content = request.getParameter("content");
 
-
-			SecSql sql = SecSql.from("UPDATE article");
+			sql = SecSql.from("UPDATE article");
 			sql.append("SET ");
 			sql.append("title = ?,", title);
 			sql.append("content = ?", content);
@@ -54,7 +76,7 @@ public class ArticleDoModifyServlet extends HttpServlet {
 			
 			DBUtil.update(conn, sql);
 
-			response.getWriter().append(String.format("<script>alert('%d번 글이 수정되었습니다.'); location.replace('detail?id=%d');</script>", id, id));
+			response.getWriter().append(String.format("<script>alert('%d번 글이 수정되었습니다.'); location.replace('list');</script>", id));
 					
 			
 
